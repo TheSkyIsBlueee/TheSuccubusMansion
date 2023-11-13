78e22898d65c358f0e86220affc12b43a59cba77
TheSkyIsBlueee <TheSkyIsBlueee@hotmail.com>
2023-11-13 00:28:24 -0400

    Abstracted the Load UI from MainMenu to LoadSaveSlot, a FragmentContentController. A FragmentContentController is a partial content controller that is not meant to run alone. Updated ActiveContentController to manage fragment controllers. Added system buttons to view.tscn - Save, Load, Quit, and Reload Scene. Added isSaveLoaded to SaveState.

--- ContentControllers/MainMenu.gd
+++ ContentControllers/MainMenu.gd
@@ -22,48 +22,7 @@ func onNewGameClick(event):
 
 func onLoadClick(event):
 	if isGuiClickEvent(event):
-		PostBoard.postLog("\n\n\n\n\n");
-		var saveSlots = SaveManager.getSaveSlots();
-		if saveSlots.size() == 0:
-			PostBoard.postLog("\nNo saves found.");
-		else:
-			for save in saveSlots:
-				var dataParts = SaveManager.splitSaveSlot(save);
-				PostBoard.postLog("\nSave " + dataParts[2] + " - " + SaveManager.saveTimeToDateTime(String(dataParts[1])) + " | ");
-				PostBoard.postInteractableLog("Load", _onLoadSaveClick.bind(save));
-				PostBoard.postLog(" | ");
-				PostBoard.postInteractableLog("Delete", _onDeleteSaveClick.bind(save));
-
-		PostBoard.postInteractableLog("\nBack", _onLoadBackClick);
-
-func _onLoadSaveClick(event, save: String):
-	if isGuiClickEvent(event):
-		SaveManager.loadSave(save);
-
-func _onDeleteSaveClick(event, save: String):
-	if isGuiClickEvent(event):
-		PostBoard.clear();
-		var dataParts = SaveManager.splitSaveSlot(save);
-		PostBoard.postLog("\nSave " + dataParts[2] + " - " + SaveManager.saveTimeToDateTime(String(dataParts[1])) + " | ");
-		PostBoard.postLog("\nAre you sure you want to delete this save? This can not be reversed.");
-		PostBoard.postInteractableLog("\nConfirm", _onDeleteConfirmClick.bind(save));
-		PostBoard.postLog(" | ");
-		PostBoard.postInteractableLog("Cancel", _onDeleteCancelClick);
-
-func _onDeleteConfirmClick(event, save: String):
-	if isGuiClickEvent(event):
-		SaveManager.deleteSave(save);
-		PostBoard.clear();
-		onLoadClick(event);
-
-func _onDeleteCancelClick(event):
-	if isGuiClickEvent(event):
-		PostBoard.clear();
-		onLoadClick(event);
-
-func _onLoadBackClick(event):
-	if isGuiClickEvent(event):
-		run();
+		ActiveContentController.addFragmentController(load("res://FragmentContentControllers/LoadSaveSlot.gd").new());
 
 func onExitClick(event):
 	if isGuiClickEvent(event):
--- Scripts/MainController.gd
+++ Scripts/MainController.gd
@@ -1,12 +1,51 @@
 extends Control
 
 const ContentController = preload("res://ContentControllers/ContentController.gd");
+const SaveManager = preload("res://utils/SaveManager.gd");
 
 var _logList: Node;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_on_cancel_quit_pressed();
 	_logList = $Row/Background/SceneLog/LogListContainer/LogList;
 	PostBoard.setPostBoard(_logList);
 	ActiveContentController.setController(load("res://ContentControllers/MainMenu.gd").new());
 
+func _on_save_pressed():
+	if SaveState.isSaveLoaded:
+		SaveManager.save();
+		PostBoard.postLog("\n\nGame saved.\n\n");
+
+func _on_load_pressed():
+	ActiveContentController.addFragmentController(load("res://FragmentContentControllers/LoadSaveSlot.gd").new());
+
+func _on_quit_pressed():
+	if !SaveState.isSaveLoaded:
+		_on_quit_anyways_pressed();
+		pass;
+
+	$Row/LeftColumn/SystemButtons/Save.hide();
+	$Row/LeftColumn/SystemButtons/Load.hide();
+	$Row/LeftColumn/SystemButtons/Quit.hide();
+	$Row/LeftColumn/SystemButtons/SaveQuit.show();
+	$Row/LeftColumn/SystemButtons/QuitAnyways.show();
+	$Row/LeftColumn/SystemButtons/CancelQuit.show();
+
+func _on_quit_anyways_pressed():
+	GlobalGetTree.quit();
+
+func _on_cancel_quit_pressed():
+	$Row/LeftColumn/SystemButtons/Save.show();
+	$Row/LeftColumn/SystemButtons/Load.show();
+	$Row/LeftColumn/SystemButtons/Quit.show();
+	$Row/LeftColumn/SystemButtons/SaveQuit.hide();
+	$Row/LeftColumn/SystemButtons/QuitAnyways.hide();
+	$Row/LeftColumn/SystemButtons/CancelQuit.hide();
+
+func _on_save_quit_pressed():
+	_on_save_pressed();
+	_on_quit_anyways_pressed();
+
+func _on_reload_scene_pressed():
+	ActiveContentController.reloadScene();
